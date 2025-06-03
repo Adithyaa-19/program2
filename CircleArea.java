12be1871228b3509eeb3a4e9b6c8ee37e62fb8bf
Your Name <you@example.com>
2025-06-03 16:46:14 +0530

    Initial commit for program 2

---
+++ CircleArea.java
@@ -0,0 +1,27 @@
+public class CircleArea {
+    private double radius;
+
+    public CircleArea(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    public void display() {
+        System.out.println("Circle Radius: " + radius);
+        System.out.println("Area: " + getArea());
+        System.out.println("Perimeter: " + getPerimeter());
+    }
+
+    public static void main(String[] args) {
+        CircleArea circle = new CircleArea(7.5);
+        circle.display();
+    }
+}
+
